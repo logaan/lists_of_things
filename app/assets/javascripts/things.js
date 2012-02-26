57564066a10e1e924e6d6e8fabb5ef4c422fbd2b
Logan Campbell-McPherson <colin@logaan.net>
2012-02-26 17:05:40 +1100

    delete and create show up on hover

--- app/assets/javascripts/things.js
+++ app/assets/javascripts/things.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+jQuery(function() {
+  $(".thing .actions").hide();
+
+  $(".thing .detail").hover(function(e) {
+    $(".actions", this).show();
+  }, function() {
+    $(".actions", this).hide();
+  });
+});
+
